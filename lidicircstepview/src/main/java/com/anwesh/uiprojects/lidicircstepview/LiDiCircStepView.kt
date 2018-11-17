9d567e82ed124dfdd8d10077e8a216d8c2edc794
Anwesh43 <anweshthecool0@gmail.com>
2018-11-17 11:11:03 +0530

    adding libraries and defining constants

--- lidicircstepview/src/main/java/com/anwesh/uiprojects/lidicircstepview/LiDiCircStepView.kt
+++ lidicircstepview/src/main/java/com/anwesh/uiprojects/lidicircstepview/LiDiCircStepView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.lidicircstepview
 /**
  * Created by anweshmishra on 17/11/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 4
+val STROKE_FACTOR : Int = 60
+val SIZE_FACTOR : Int = 3
+val COLOR : Int = Color.parseColor("#01579B")
+val BACK_COLOR : Int = Color.parseColor("#BDBDBD")
+val DELAY : Long = 30
