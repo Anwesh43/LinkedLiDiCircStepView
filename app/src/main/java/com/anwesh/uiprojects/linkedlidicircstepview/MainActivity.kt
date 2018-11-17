b87099cf0ebb5328790f9a6695ac6fcb5c1dfb74
Anwesh43 <anweshthecool0@gmail.com>
2018-11-17 11:55:05 +0530

    adding LiDiCircStepView create method

--- app/src/main/java/com/anwesh/uiprojects/linkedlidicircstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedlidicircstepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedlidicircstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.lidicircstepview.LiDiCircStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val view : LiDiCircStepView = LiDiCircStepView.create(this)
     }
 }
